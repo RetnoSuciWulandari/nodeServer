5a0ff00a3ee19662fd6b9984bfdfa7ca99c9f3d6
Retno <retnoscwulandari@gmail.com>
2019-10-16 12:14:11 +0100

    server

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 var http = require("http");
+
 const server = http.createServer((request, response) => {
-  response.writeHead(200);
-  response.end("Hello World");
+  //response.writeHead(200);
+
+  var url = request.url;
+
+  if (url === "/") {
+    response.write("Hello World");
+    response.end();
+  } else if (url === "/goodbye") {
+    response.write("Goodbye World");
+    response.end();
+  } else if (url === "/error") {
+    response.statusCode = 500;
+    response.end(JSON.stringify({ error: "server error" }));
+  }
 });
 
 server.listen(3000);
