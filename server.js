44a2c9548b2c3e9d807fec19db127549bc2e9864
Retno <retnoscwulandari@gmail.com>
2019-10-16 11:16:30 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require("http");
+const server = http.createServer((request, response) => {
+  response.writeHead(200);
+  response.end("Hello World");
+});
+
+server.listen(3000);
+//console.log("hello");
